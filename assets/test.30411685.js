4172984f5389d31a4f94f87c8f58fd0724bcdc40
18908360652 <362850915@qq.com>
2023-03-20 15:16:34 +0800

    123

---
+++ assets/test.30411685.js
@@ -0,0 +1,1 @@
+import{r as n,t as o,o as c,c as l,a as t,d as r,f as u,F as d}from"./index.af807584.js";const f={__name:"test",setup(m){let a=n({count:1,name:"\u5F20\u4E09"}),{count:e}=o(a);const s=()=>{e.value++,console.log(e.value,keys)};return(_,i)=>(c(),l(d,null,[t("div",null,"num:"+r(u(e)),1),t("button",{onClick:s},"add")],64))}};export{f as default};
